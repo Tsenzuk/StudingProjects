e075a57de334588451ef61472a340928be187126
Serg <tsenzuk@gmail.com>
2015-11-10 10:29:10 +0000

    + added mongoose and koa-router as dependencies

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var koa = require('koa');
-var app = koa();
+var Router = require('koa-router');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/projects');
 
+var app = koa();
+var projectsRouter = new Router({
+  prefix: '/projects'
+});
+
+projectsRouter.get("/",function *(next){
+    this.body = '/projects/';
+});
+app.use(projectsRouter.routes());
 app.use(function* (){
   this.body = 'Hello World';
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
