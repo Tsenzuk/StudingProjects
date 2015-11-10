2236bbaad0c95ab99ad74ef6850f79f786c50c63
Serg <tsenzuk@gmail.com>
2015-11-10 17:26:23 +0000

    + views for projects view and edit + router for projects + bootstrap support

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 var koa = require('koa');
-var Router = require('koa-router');
+var serve = require('koa-static');
 var mongoose = require('mongoose');
+var Jade = require('koa-jade')
+var jade = new Jade({viewPath: './lib/views/'});
+
+mongoose.connection.on('error', function(err){
+    console.log(err);
+});
 mongoose.connect('mongodb://localhost/projects');
 
 var app = koa();
-var projectsRouter = new Router({
-  prefix: '/projects'
-});
+var projectsRouter = require("./lib/routes/projects");
 
-projectsRouter.get("/",function *(next){
-    this.body = '/projects/';
-});
+app.use(jade.middleware)
+app.use(serve('./node_modules/jquery/dist/'));
+app.use(serve('./node_modules/bootstrap/dist/'));
 app.use(projectsRouter.routes());
 app.use(function* (){
   this.body = 'Hello World';
