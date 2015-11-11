b2d8a494cb433cfc8f2305cd226dc75d54557181
Serg <tsenzuk@gmail.com>
2015-11-11 15:24:49 +0000

    + added delete page + added appropriate navigation links

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 var koa = require('koa');
 var serve = require('koa-static');
 var mongoose = require('mongoose');
-var Jade = require('koa-jade')
-var jade = new Jade({viewPath: './lib/views/'});
+var Jade = require('koa-jade');
+var koaBody = require('koa-body');
+var jade = new Jade({
+  viewPath: './lib/views/'
+});
 
-mongoose.connection.on('error', function(err){
-    console.log(err);
+mongoose.connection.on('error', function(err) {
+  console.log(err);
 });
 mongoose.connect('mongodb://localhost/projects');
 
 var app = koa();
 var projectsRouter = require("./lib/routes/projects");
 
-app.use(jade.middleware)
+app.use(jade.middleware);
+app.use(koaBody({
+  formidable: {
+    uploadDir: __dirname
+  }
+}));
 app.use(serve('./node_modules/jquery/dist/'));
 app.use(serve('./node_modules/bootstrap/dist/'));
 app.use(projectsRouter.routes());
-app.use(function* (){
+app.use(function*() {
   this.body = 'Hello World';
 });
 
